26c3d0010766edaf93772d72c950fea2d2f223fb
Siegerus <ivanoffsiegerus@gmail.com>
2024-07-22 20:20:21 +0300

    lesson 2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 "use sttrict"
 
-let money = prompt("Ваш бюджет на месяц?");
-let time = prompt("Введите дату в формате YYYY-MM-DD");
+let money = +prompt("Ваш бюджет на месяц?", '');
+let time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
     budget : money,
@@ -13,14 +13,62 @@ let appData = {
     savings: false 
 };
 
-let firstAnswer = prompt("Введите обязательную статью расходов в этом месяце");
-let firstAnswerValue = prompt ("Во сколько обойдется?");
-let secondAnswer = prompt("Введите обязательную статью расходов в этом месяце");
-let secondAnswerValue = prompt ("Во сколько обойдется?");
+for (let i = 0; i < 2; i++) {       // цикл, что бы задать вопросы 2 раза
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null
+        && a != '' && b != '' && a.length < 50) { // если полученныые данные строка и переменные не нал, и длинна строки не больше 50 - то ---
+        console.log("done");
+        appData.expenses[a] = b;       // устанавливаем св-ва для объекта. Получится пара ключ-значение
+    } else {
+        console.log ("bad result");
+        i--;
+        }
+    };
 
-appData.expenses[firstAnswer] = firstAnswerValue;
-appData.expenses[secondAnswer] = secondAnswerValue;
+let q = 0;
+while (q < 2) {
+    let a = prompt("second Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("second Во сколько обойдется?", '');
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null
+        && a != '' && b != '' && a.length < 50) {
+        console.log("second done");
+        appData.expenses[a] = b;
+    } else {
+        console.log("bad result");
+        q--;
+    }
+    q++
+};
 
-alert(appData.budget / 30);
+let w = 0;
+do {
+    let a = prompt("third Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("third Во сколько обойдется?", '');
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null
+        && a != '' && b != '' && a.length < 50) {
+        console.log("third done");
+        appData.expenses[a] = b;
+    } else {
+        console.log("bad result");
+        w--;
+    }
+    w++
+}
+while (w < 2);
 
-console.log(appData);
+appData.moneyPerDay = appData.budget / 30; // создаём новое св-во для объекта
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log ("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
+
+/* console.log(appData.expenses); */
